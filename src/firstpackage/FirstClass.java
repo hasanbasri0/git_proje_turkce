3b726d505b26bc2eb9739fbda5466c50f1e3fa7e
hasanbasri0 <67021744+hasanbasri0@users.noreply.github.com>
2020-06-20 22:31:36 +0300

    Update FirstClass.java
    değişiklik yapalim mi?

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 		
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
-		System.out.println("third commit from local repository");
+		System.out.println("3.degişiklik yapalim mi?");
 		System.out.println("first commit from remote repository");
 
 	}
