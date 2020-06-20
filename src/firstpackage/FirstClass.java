d0def4644bcc987e567979909dbea27fd784e953
hasanbasri <hasanbasriuysal3@gmail.com>
2020-06-20 22:37:12 +0300

    conflict resolved / sonuc cözüldü

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -5,11 +5,10 @@ public class FirstClass {
 		
 		System.out.println("first commit from local repository");
 		System.out.println("second commit from local repository");
-<<<<<<< HEAD
-		System.out.println("ücüncü commit local repositoryden");
-=======
+
+		
 		System.out.println("3.degiÅŸiklik yapalim mi?");
->>>>>>> refs/remotes/origin/master
+
 		System.out.println("first commit from remote repository");
 
 	}
